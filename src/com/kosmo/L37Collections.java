cf3e2c4aef00447ac8c92b10b54e415f1330bd53
kyoungmin <develckm86@gmail.com>
2025-07-15 12:32:32 +0900

    이터레이터 반복자

--- src/com/kosmo/L37Collections.java
+++ src/com/kosmo/L37Collections.java
@@ -1,9 +1,6 @@
 package com.kosmo;
 //java.util : 개발자에게 유용한 도구(라이브러리, class)를 제공
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 
 public class L37Collections {
     public static void main(String[] args) {
@@ -73,6 +70,28 @@ public class L37Collections {
         System.out.println(listStr);
         listStr.sort(null);
         System.out.println(listStr);
+        //contains,sort,remove,add : List 유용한 함수
 
+
+        for(String s : listStr){ //index를 알수 없다.
+            System.out.println(s);
+        }
+        System.out.println("향상된 for 반복문 (iterator) 종료");
+        for (String s : listStr){
+            System.out.println(s);
+        }
+        System.out.println("향상된 for 반복문은 iterator를 초기화하고 다시 사용");
+
+        Iterator<String> it=listStr.iterator();
+        while (it.hasNext()){
+            String s=it.next();
+            System.out.println(s);
+        }
+        //it=listStr.iterator();
+        System.out.println("while 로 이터레이터(반복자) 호출");
+        while (it.hasNext()){ //반복자는 끝으로 이동하면 다시 사용할 수없음
+            String s=it.next();
+            System.out.println(s);
+        }
     }
 }
