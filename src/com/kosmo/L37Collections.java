e70e038b327de1f0bb6ba3e0e8a677d3ec01a0d7
kyoungmin <develckm86@gmail.com>
2025-07-15 11:33:31 +0900

    collections List 수업

---
+++ src/com/kosmo/L37Collections.java
@@ -0,0 +1,78 @@
+package com.kosmo;
+//java.util : 개발자에게 유용한 도구(라이브러리, class)를 제공
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class L37Collections {
+    public static void main(String[] args) {
+        int [] arr=new int[4];//{0,0,0,0}
+        System.out.println(Arrays.toString(arr));
+
+        //arr[4]=1; //{0,0,0,0,1} 기대 중 => 오류
+        //배열 : 순서와 길이가 있는 데이터
+        //Array [] : 처음 만들어진 길이 고정
+
+
+        List list=new ArrayList();
+        //List : 길이가 변경되는 배열?
+        list.add(10);
+        list.add(10.0);
+        list.add(10.0f);
+        list.add(10l);
+        System.out.println(list);
+        System.out.println(list.size()); //arr.length
+        System.out.println(list.get(3)); //list의 인덱스 참조, arr[3]
+        //System.out.println(list.get(4)); //Array 처럼 없는 것은 참조 불능
+        list.add("10");
+        System.out.println(list.size());
+        System.out.println(list);
+        System.out.println(list.remove("10")); //데이터를 찾아서 지움(실패가능)
+        System.out.println(list.remove("10.0")); //데이터를 찾아서 지움(실패가능)
+        System.out.println(list.remove(0));//인덱스로 지울때는 삭제한 대상이 출력
+        System.out.println(list);
+        System.out.println(list.contains(10.0));
+        System.out.println(list.contains(11));
+        //(double) 10; : 수(기본형)를 더블에 맞게 변경
+        //(Double) 10; : 객체(자료형)의 타입이 Double이 있냐
+
+        Object [] objArr={10,10.0,10.0f,10l};
+        //10.0
+        //objArr.contains(10.0);//불가능 => 직접 탐색
+        boolean result=false;
+        for (int i=0; i<objArr.length; i++){
+            if((objArr[i] instanceof Double) && 10.0 == (Double) objArr[i]){
+                result=(true);
+                break;
+            }
+        }
+        //Array 사용 이유 : 메모리가 절약되서 !!(고급언어일수록 Array가 없는 경우가 존재합니다.)
+
+
+        List<Integer> listInt=new ArrayList<>();
+        listInt.add(10);
+        //listInt.add(10.0);
+        //listInt.add("10");
+        listInt.add(99);
+        listInt.add(20);
+        listInt.add(-100);
+        System.out.println(listInt);
+        listInt.sort(null); //오름차순  작은수=>큰수로
+        System.out.println(listInt);
+        listInt.sort(Comparator.reverseOrder());
+        System.out.println(listInt);
+        //문자열도 정렬이되나?? (문자=>번호)
+        List<String> listStr=new ArrayList<>();
+        listStr.add("Banana");
+        listStr.add("사과");
+        listStr.add("Kiwi");
+        listStr.add("Lemon");
+        listStr.add("apple");
+        listStr.add("Apple"); //대문자가 앞번호 소문자가 뒷번호
+        System.out.println(listStr);
+        listStr.sort(null);
+        System.out.println(listStr);
+
+    }
+}
