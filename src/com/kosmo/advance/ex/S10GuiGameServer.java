47455481a4327f9d417a0ec2e983b3b0a06b6aed
develckm86 <develckm86@gmail.com>
2025-07-24 23:26:32 +0900

    블럭 멀티게임

--- src/com/kosmo/advance/ex/S10GuiGameServer.java
+++ src/com/kosmo/advance/ex/S10GuiGameServer.java
@@ -6,29 +6,40 @@ import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class S10GuiGameServer {
-    public static List<Socket> clients=new ArrayList<>();
-    public static List<ObjectOutputStream> clientsOut=new ArrayList<>();
-
+    public static Map<Socket,ObjectOutputStream> clients=new HashMap<>();
+    //public static List<ObjectOutputStream> clientsOut=new ArrayList<>();
+    public static Map<String,Block> blocks=new HashMap<>();
 
     public static void main(String[] args) {
         try (ServerSocket server=new ServerSocket(5555)){
+            System.out.println("서버시작");
             while (true){
                 Socket client=server.accept();
-                clients.add(client);
                 ObjectOutputStream clientOut=new ObjectOutputStream(client.getOutputStream());
-                clientsOut.add(clientOut);
+                clients.put(client,clientOut);
+                for (Map.Entry entry: blocks.entrySet()){//최초 접속자에게 모든 블럭 보내기
+                    Block b=(Block)entry.getValue();
+                    clientOut.writeObject(b);
+                    clientOut.flush();
+                }
                 //client가 보내는 입력을 받아서 다른 클라이언트 모드에게 보내기
                 new Thread(()->{
                     try {
                         ObjectInputStream input=new ObjectInputStream(client.getInputStream());
                         while (true){
-                            Object blockObj=input.readObject();
-                            System.out.println(client.getInetAddress().getAddress()+"님 오브젝트 전송");
-                            //접속한 모든 사람에게 블럭 보내기
-                            for(ObjectOutputStream out: clientsOut){
+                            Block blockObj=(Block) input.readObject();
+                            System.out.println(blockObj);
+                            blocks.put(blockObj.id, blockObj); //받은 블럭들 저장하기
+                            //접속한 모든 사람에게 블럭 보내기(자신 제외)
+                            for (Map.Entry<Socket,ObjectOutputStream> entry:clients.entrySet()){
+                                Socket socket=entry.getKey();
+                                ObjectOutputStream out=entry.getValue();
+                                if(socket.equals(client)){continue;}
                                 out.writeObject(blockObj);
                                 out.flush(); //버퍼 비우기
                             }
@@ -41,7 +52,6 @@ public class S10GuiGameServer {
                         e.printStackTrace();
                     }finally {
                         clients.remove(client);
-                        clientsOut.remove(clientOut);
                     }
                 }).start();
             }
